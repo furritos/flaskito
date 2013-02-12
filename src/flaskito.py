78b6637023757241fdcf8c6e5af7bf33df5c8c9e
tim hsu <tim.yellow@gmail.com>
2013-02-12 21:01:45 +0800

    update

--- src/flaskito.py
+++ src/flaskito.py
@@ -1,9 +1,19 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Flaskito rulastonico running on a Java web container."
+    return "New Flaskito rulastonico running on a Java web container."
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name='sample'):
+    return render_template('hello.html', name=name)
     
+@app.route('/hello2/')
+@app.route('/hello2/<name>')
+def hello(name='sample'):
+    return render_template('hello2.html', name=name)
 if __name__ == "__main__":
     app.run()
