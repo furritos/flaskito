cd633c4767c8557354e4873c453524d1b6a9d891
carlos <carlos@twobitcoder.com>
2016-01-04 01:35:20 -0800

    Added sqlalchemy and flask-sqlalchemy for POC. flaskito.py was updated to include test queries against Oracle 11g database via ojdbc6.jar.

--- src/flaskito.py
+++ src/flaskito.py
@@ -1,6 +1,60 @@
 from flask import Flask
 from flask import render_template
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Sequence
+from datetime import datetime
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'oracle+zxjdbc://dojtest:oracle@10.1.0.201:31521/XE'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True  
+
+db = SQLAlchemy(app)
+
+class Post(db.Model):
+    id = db.Column(db.Integer, Sequence('post_id_seq'), primary_key=True)
+    title = db.Column(db.String(80))
+    body = db.Column(db.Text)
+    pub_date = db.Column(db.DateTime)
+
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
+    category = db.relationship('Category',
+        backref=db.backref('posts', lazy='dynamic'))
+
+    def __init__(self, title, body, category, pub_date=None):
+        self.title = title
+        self.body = body
+        if pub_date is None:
+            pub_date = datetime.utcnow()
+        self.pub_date = pub_date
+        self.category = category
+
+    def __repr__(self):
+        return '<Post %r>' % self.title
+
+
+class Category(db.Model):
+    id = db.Column(db.Integer, Sequence('catg_id_seq'), primary_key=True)
+    name = db.Column(db.String(50))
+
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return '<Category %r>' % self.name
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, Sequence('user_id_seq'), primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    email = db.Column(db.String(120), unique=True)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 @app.route("/")
 def index():
@@ -15,6 +69,33 @@ def hello(name='sample'):
 @app.route('/hello2/<name>')
 def hello2(name='sample2'):
     return render_template('hello2.html', name=name)
+
+db.drop_all()
+db.create_all()
+
+admin = User('admin', 'admin@example.com')
+guest = User('guest', 'guest@example.com')
+
+db.session.add(admin)
+db.session.add(guest)
+db.session.commit()
+
+py = Category('Python')
+p = Post('Hello Python!', 'Python is pretty cool', py)
+db.session.add(py)
+db.session.add(p)
+db.session.commit()
+
+print User.query.filter(User.email.endswith('@example.com')).all()
+
+me = User('me', 'me@example.com')
+db.session.add(me)
+db.session.commit()
+
+print me.id
+
+db.session.delete(me)
+db.session.commit()
 
 if __name__ == "__main__":
     app.run()
